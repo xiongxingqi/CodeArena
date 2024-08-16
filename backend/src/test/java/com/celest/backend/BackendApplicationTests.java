ccd53e6f33cca09e537c4504c3c551cdd33da14d
celest <1269504317@qq.com>
2024-08-16 13:01:00 +0800

    项目初始化

---
+++ backend/src/test/java/com/celest/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celest.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
